a5e07a15ddafe4ff6b92faf2849f90b5498378a6
oecorrechag <oecorrechag@unal.edu.co>
2023-02-19 11:56:45 -0500

    clase 4

--- curso_básico_de_javascript/notebooks/Clase 4/Clase4.js
+++ curso_básico_de_javascript/notebooks/Clase 4/Clase4.js
@@ -1,10 +1,17 @@
-var name = "Vane";
+var nombre = "Vane";
+console.log(typeof(nombre));
+
 var edad = 23;
+console.log(typeof(edad));
+
 var elemento = ["computadora", "celular"];
+console.log(typeof(elemento));
+
 var persona = {
     nombre: "Paola",
     edad: 24
 }
 persona;
+console.log(typeof(persona));
 
-elemento[1]
+console.log('Elemento: ' + elemento[1]);
