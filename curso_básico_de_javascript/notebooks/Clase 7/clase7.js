7766f23b98528e8183ea388ec8053476d30e4a6f
oecorrechag <oecorrechag@unal.edu.co>
2023-02-19 12:41:36 -0500

    clase 6 y 7

---
+++ curso_básico_de_javascript/notebooks/Clase 7/clase7.js
@@ -0,0 +1,12 @@
+function solution(valor) {
+    // Escribe tu código aquí 👈
+    return typeof valor;
+  }
+
+solution(1)
+solution("Dieguillo")
+solution(true)
+
+console.log("Funcion solucion sobre 1: " + solution(1));
+console.log("Funcion solucion sobre Dieguillo: " + solution("Dieguillo"));
+console.log("Funcion solucion sobre tru: " + solution(true));
