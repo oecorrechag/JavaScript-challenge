39dc3ab6c05d138c454018fcc8dffa80bf7147e5
oecorrechag <oecorrechag@unal.edu.co>
2023-02-20 17:59:10 -0500

    clase 10

---
+++ curso_básico_de_javascript/notebooks/Clase 10/clase10.js
@@ -0,0 +1,14 @@
+var a = 4 + "7";
+var b = 4 * "7";
+
+console.log(a + " " + typeof a);
+console.log(b + " " + typeof b);
+
+var c = 20;
+var d = c + "";
+var e = String(c);
+var f = Number(e);
+
+console.log(c + " " + typeof c);
+console.log(e + " " + typeof e);
+console.log(f + " " + typeof f);
---
+++ curso_básico_de_javascript/notebooks/Clase 9/clase9.js
@@ -0,0 +1,11 @@
+console.log(miNombre);
+
+// var miNombre = "Oscar";
+
+hey();
+
+function hey(){
+    console.log("Hola " + miNombre);
+}
+
+var miNombre = "Oscar";
