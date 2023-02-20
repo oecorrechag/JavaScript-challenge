5312ed7165ad48a674a9203a6a4507955a21f6bd
oecorrechag <oecorrechag@unal.edu.co>
2023-02-20 18:07:27 -0500

    clase12

---
+++ curso_básico_de_javascript/notebooks/Clase 12/clase12.js
@@ -0,0 +1,96 @@
+
+// Suma
+2 + 3 // 5
+// Resta
+5 - 3 // 2
+// Multiplicación
+4 * 2 // 8
+// División
+6 / 2 // 3
+
+//Residuo
+21 % 5 // 1
+
+"Hola" + "Oscar" // "Hola Oscar"
+console.log("Hola" + "Oscar")
+
+variable++
+variable--
+++variable
+--variable
+
+var a = 3
+var b = 3
+
+console.log(a++) //3
+console.log(++b) //4
+console.log(a) //4
+console.log(b) //4
+
+var saludo = "Hola Mundo"
+
+var contador = 1
+contador = contador + 1
+contador = contador + 1
+console.log(contador) // 3
+
+var contador = 1
+
+contador += 2
+contador -= 1
+contador *= 5
+contador /= 2
+
+console.log(contador)
+
+
+//Igualdad
+"3" == 3 // true
+3 == 3 // true
+
+// Igualdad estricta
+"3" === 3 // false
+3 === 3 // true
+
+
+//Desigualdad
+"3" != 3 // false
+3 != 3 // false
+
+// Desigualdad estricta
+"3" !== 3 // true
+3 !== 3 // false
+
+
+// Menor que
+3 < 5 // true
+
+// Mayor que
+3 > 5 // false
+
+// Mayor o igual a que
+3 >= 3 // true
+3 >= 5 // false
+
+// Menor o igual a que
+3 <= 3 // true
+3 <= 5 // true
+
+
+var a = 15
+var b = 5
+
+(a >= 10) && (a <= 20) // true
+(b >= 10) && (b <= 20) // false
+
+(a <= 10) || (a >= 20) // false
+(b <= 10) || (b >= 20) // true
+
+var a = 5
+
+!(a < 0) // true
+
+
+
+
+
