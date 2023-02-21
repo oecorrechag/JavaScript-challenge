279826327caf48c9b2eac2f8043889066a5fcae8
oecorrechag <oecorrechag@unal.edu.co>
2023-02-20 22:14:04 -0500

    clase 15

---
+++ curso_básico_de_javascript/notebooks/Clase 15/clase15.js
@@ -0,0 +1,15 @@
+var edad = 18;
+
+switch (true){
+    case edad < 18:
+        console.log("tienes " + edad);
+        break;
+    case edad == 18:
+        console.log("feliz cumple");
+        break;
+    case edad > 18:
+        console.log("tienes " + edad)
+        break;
+    default:
+        console.log("sorry error")
+}
