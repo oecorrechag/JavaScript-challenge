d6ee8f7c0f50e28e58b2bd2e799b7048ac73be13
oecorrechag <oecorrechag@unal.edu.co>
2023-02-20 22:27:12 -0500

    reto clase 15

---
+++ curso_básico_de_javascript/notebooks/Clase 15/reto.js
@@ -0,0 +1,35 @@
+function juega(lanzamiento1, lanzamiento2){
+    
+    var op1 = "piedra";
+    var op2 = "papel";
+    var op3 = "tijera";
+
+    switch (true){
+
+        case usuario == op1 && pc == op2:
+            console.log("Gana pc");
+            break;
+        case usuario == op1 && pc == op3:
+            console.log("Ganaste");
+            break;
+        case usuario == op2 && pc == op1:
+            console.log("Ganaste");
+            break;
+        case usuario == op2 && pc == op3:
+            console.log("Gana pc");
+            break;
+        case usuario == op3 && pc == op1:
+            console.log("Gana pc");
+            break;
+        case usuario == op3 && pc == op2:
+            console.log("Ganaste");
+            break;
+        default:
+            console.log("Empate!!!");
+    }
+}
+
+var usuario = 'piedra';
+var pc = 'tijera';
+
+juega(usuario, pc)
