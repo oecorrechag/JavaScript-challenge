63767a4da9f448d1e1c78f4a7c72f1b36b30570d
oecorrechag <oecorrechag@unal.edu.co>
2023-02-21 15:36:58 -0500

    propiedades de los objetos

--- curso_básico_de_javascript/notebooks/Clase22-objetos/clase22.js
+++ curso_básico_de_javascript/notebooks/Clase22-objetos/clase22.js
@@ -1,4 +1,4 @@
-var miCar = {
+var myCar = {
     brand: "Mazda",
     model: "3",
     year: 2021,
@@ -7,8 +7,19 @@ var miCar = {
     }
 }
 
-console.log(miCar)
-console.log(miCar.brand)
-console.log(miCar.detallecar())
+console.log(myCar)
+console.log(myCar["model"])
+console.log(myCar.brand)
+console.log(myCar.detallecar())
 
+var propiedad1 = "year"
+console.log(myCar[propiedad1])
+
+// agregar una propiedad
+myCar.color = "gray machine"
+console.log(myCar)
+
+// Cambiar propiedad
+myCar.model = "Mazda 3"
+console.log(myCar)
 
