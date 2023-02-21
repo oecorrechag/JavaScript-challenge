7375d286128c3bda738f199be679ee1e0025f496
oecorrechag <oecorrechag@unal.edu.co>
2023-02-20 20:18:36 -0500

    clase 13

---
+++ curso_básico_de_javascript/notebooks/Clase 13/clase13.js
@@ -0,0 +1,12 @@
+function solution(secreto) {
+    // Escribe tu código aquí 👈
+    return secreto > 5;
+  }
+
+solution(1)
+solution(5)
+solution(10)
+
+console.log("Funcion solucion sobre 1: " + solution(1));
+console.log("Funcion solucion sobre 5: " + solution(5));
+console.log("Funcion solucion sobre 10: " + solution(10));
