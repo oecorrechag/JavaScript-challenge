9c10febb2cdc4989201a9d19fe5c60fcba97363e
oecorrechag <oecorrechag@unal.edu.co>
2023-02-21 19:33:23 -0500

    clase 28

---
+++ curso_básico_de_javascript/notebooks/Clase28/clase28.js
@@ -0,0 +1,30 @@
+// En este desafío debes ayudarnos a arreglar la función solution.
+// Esta recibe un array cars y debería retornar el array filtrado únicamente con los objetos que tengan la 
+// propiedad licensePlate.
+// Pero no está funcionando.
+// La función solo devuelve un objeto con el primer elemento que cumple los requisitos, no estamos recibiendo 
+// los demás elementos del array que también deberían pasar el filtro.
+// ¿Nos ayudas? ¿Cómo solucionarías el error?
+
+function solution(cars) {
+    return cars.filter(car => car.licensePlate);
+  }
+
+const cars = [
+{
+    color: 'red',
+    brand: 'Kia',
+},
+{
+    color: 'gray',
+    brand: 'Chevrolet',
+    licensePlate: 'AAA111',
+},
+{
+    licensePlate: 'RGB255',
+},
+];
+
+solution(cars);
+
+console.log('Find cars with plate: ', solution(cars));
